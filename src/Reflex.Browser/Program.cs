435e289e9d04058bd02d5dd3616aa10f7df43940
Peter Åslund <peter.aslund@consid.se>
2019-03-11 12:04:52 +0100

    Arguments

--- src/Reflex.Browser/Program.cs
+++ src/Reflex.Browser/Program.cs
@@ -20,55 +20,71 @@ namespace Reflex.Browser
 
 		static void Main(string[] args)
 		{
-			if (args == null || args.Length <= 0)
+			var argsSupplied = args != null && args.Length > 0;
+			if (!argsSupplied)
 			{
 				Log($"Usage: {REFLEX_ASSEMBLY}.exe/.dll {{FILEPATH}} {{TYPENAME}}");
 			}
-			else
+
+			var filePath = args?.Length > 0 ? args[0] : GetInputArg($"Enter {{FILEPATH}}: ");
+			var typename = args?.Length > 1 ? args[1] : GetInputArg($"Enter {{TYPENAME}}: ");
+
+			try
 			{
-				try
-				{
-					var filePath = args[0];
-					var typename = args[1];
-
-					Log("ARGS:");
-					Log($"FilePath: {filePath}");
-					Log($"TypeName: {typename}");
+				Log("ARGS:");
+				Log($"FilePath: {filePath}");
+				Log($"TypeName: {typename}");
 
 
-					var loader = new AssemblyFileLoader();
+				var loader = new AssemblyFileLoader();
 
-					Log("Loading assembly...");
-					var assembly = loader.Load(filePath);
-					Log(assembly);
+				Log("Loading assembly...");
+				var assembly = loader.Load(filePath);
+				Log(assembly);
 
-					Log("Getting type...");
-					var type = assembly.GetType(typename, true);
-					Log(type);
-
-
-					Log("Creating instance...");
-					var instance = Activator.CreateInstance(type);
-					Log(instance);
-				}
-				catch (Exception ex)
-				{
-					Log($"Exception was thrown!");
-					Log(ex);
-				}
+				Log("Getting type...");
+				var type = assembly.GetType(typename, true);
+				Log(type);
 
 
-				if (Environment.UserInteractive)
-				{
-					Log("Press [ENTER] to exit...");
-					Console.ReadLine();
-				}
+				Log("Creating instance...");
+				var instance = Activator.CreateInstance(type);
+				Log(instance);
+			}
+			catch (Exception ex)
+			{
+				Log($"Exception was thrown!");
+				Log(ex);
+			}
+
+			if (Environment.UserInteractive)
+			{
+				Log("Press [ENTER] to exit...");
+				Console.ReadLine();
 			}
 		}
 
 		private static void Log(object message, bool format = true)
 		{
 			_logHelper.Log(message, format);
+		}
+
+		private static string GetInputArg(string message, Func<string, bool> validate = null)
+		{
+			if (validate == null)
+			{
+				validate = (s) => !string.IsNullOrWhiteSpace(s);
+			}
+
+			string result = null;
+			var valid = false;
+			while (!valid)
+			{
+				Console.WriteLine(message);
+				result = Console.ReadLine();
+				valid = validate(result);
+			}
+			return result;
 		}
 	}
 }
