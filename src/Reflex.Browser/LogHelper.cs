269cbfa4d7311eb28e9eb98ee82a069314bc3897
Peter Åslund <peter.aslund@consid.se>
2019-03-11 13:07:48 +0100

    LogMultiline

--- src/Reflex.Browser/LogHelper.cs
+++ src/Reflex.Browser/LogHelper.cs
@@ -15,55 +15,64 @@ namespace Reflex.Browser
 			_logger = loggerFactory.CreateLogger(GetType());
 		}
 
-		public void Log(object message, LogLevel? logLevel = null, bool format = true)
+		public void Log(object message, LogLevel? logLevel = null, bool format = true, bool logEmpty = true)
 		{
 			string formatted;
 			if (message is Exception ex)
 			{
+				if (!logLevel.HasValue)
+					logLevel = LogLevel.Warning;
+
 				formatted = $"Exception to string: {ex}";
 				var ext = ex.GetType().Name;
 
 				if (ex is FileLoadException fileLoad)
 				{
-					Log($"{ext} at fileName: {fileLoad.FileName}");
-					Log($"FusionLog: {fileLoad.FusionLog}");
+					Log($"{ext} at fileName: {fileLoad.FileName}", logLevel);
+
+					Log($"FusionLog :BEGIN:", logLevel);
+					LogMultiline(fileLoad.FusionLog, logLevel);
+					Log($"FusionLog :END:", logLevel);
 				}
 				if (ex is FileNotFoundException fileNotFound)
 				{
-					Log($"{ext} at fileName: {fileNotFound.FileName}");
-					Log($"FusionLog: {fileNotFound.FusionLog}");
+					Log($"{ext} at fileName: {fileNotFound.FileName}", logLevel);
+
+					Log($"FusionLog :BEGIN:", logLevel);
+					LogMultiline(fileNotFound.FusionLog, logLevel);
+					Log($"FusionLog :END:", logLevel);
 				}
 				if (ex is ReflectionTypeLoadException reflectionTypeLoad)
 				{
-					Log($"{ext} with types: {reflectionTypeLoad.Types.Length}");
+					Log($"{ext} with types: {reflectionTypeLoad.Types.Length}", logLevel);
 
-					Log($"LoaderExceptions :: {reflectionTypeLoad.LoaderExceptions.Length}");
+					Log($"LoaderExceptions :: {reflectionTypeLoad.LoaderExceptions.Length}", logLevel);
 					for (var i = 0; i < reflectionTypeLoad.LoaderExceptions.Length; i++)
 					{
 						var loadEx = reflectionTypeLoad.LoaderExceptions[i];
-						Log($"LoadExc {i + 1}/{reflectionTypeLoad.LoaderExceptions.Length} :BEGIN:");
-						Log(loadEx);
-						Log($"LoadExc {i + 1}/{reflectionTypeLoad.LoaderExceptions.Length} :END:");
+						Log($"LoadExc {i + 1}/{reflectionTypeLoad.LoaderExceptions.Length} :BEGIN:", logLevel);
+						Log(loadEx, logLevel);
+						Log($"LoadExc {i + 1}/{reflectionTypeLoad.LoaderExceptions.Length} :END:", logLevel);
 					}
 				}
 
 
 				if (ex is AggregateException agg)
 				{
-					Log($"AggregateExceptions :: {agg.InnerExceptions.Count}");
+					Log($"AggregateExceptions :: {agg.InnerExceptions.Count}", logLevel);
 					for (var i = 0; i < agg.InnerExceptions.Count; i++)
 					{
 						var aggInner = agg.InnerExceptions[i];
-						Log($"AggExc {i + 1}/{agg.InnerExceptions.Count} :BEGIN:");
-						Log(aggInner);
-						Log($"AggExc {i + 1}/{agg.InnerExceptions.Count} :END:");
+						Log($"AggExc {i + 1}/{agg.InnerExceptions.Count} :BEGIN:", logLevel);
+						Log(aggInner, logLevel);
+						Log($"AggExc {i + 1}/{agg.InnerExceptions.Count} :END:", logLevel);
 					}
 				}
 				else if (ex.InnerException != null)
 				{
-					Log($"InnerException :BEGIN:");
-					Log(ex.InnerException);
-					Log($"InnerException :END:");
+					Log($"InnerException :BEGIN:", logLevel);
+					Log(ex.InnerException, logLevel);
+					Log($"InnerException :END:", logLevel);
 				}
 			}
 			else
@@ -72,18 +81,34 @@ namespace Reflex.Browser
 			}
 
 
-			if (!string.IsNullOrWhiteSpace(formatted))
+			if (string.IsNullOrWhiteSpace(formatted) && !logEmpty)
 			{
-				if (!logLevel.HasValue)
-				{
-					if (formatted.StartsWith("WARN"))
-						logLevel = LogLevel.Warning;
-					else if (formatted.StartsWith("ERR"))
-						logLevel = LogLevel.Error;
-					else
-						logLevel = LogLevel.Information;
-				}
-				_logger.Log(logLevel.Value, formatted, args: null);
+				return;
+			}
+
+			if (!logLevel.HasValue && string.IsNullOrWhiteSpace(formatted))
+				logLevel = LogLevel.Information;
+
+			if (!logLevel.HasValue)
+			{
+				if (formatted.StartsWith("WARN") ||
+					formatted.StartsWith("WRN"))
+					logLevel = LogLevel.Warning;
+				else if (formatted.StartsWith("ERROR") ||
+						 formatted.StartsWith("ERR"))
+					logLevel = LogLevel.Error;
+				else
+					logLevel = LogLevel.Information;
+			}
+			_logger.Log(logLevel.Value, formatted, args: null);
+		}
+
+		public void LogMultiline(string message, LogLevel? logLevel = null)
+		{
+			var lines = message.Split(Environment.NewLine.ToCharArray());
+			foreach (var line in lines)
+			{
+				Log(line, logLevel: logLevel, format: true, logEmpty: true);
 			}
 		}
 	}
